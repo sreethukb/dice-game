eb81949ba5994656a4fda8de5297c9fc3865ad40
Sreethu K Binu <101237839+sreethukb@users.noreply.github.com>
2024-06-06 19:07:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".dice .img1").setAttribute("src", "./images/dice"+randomNumber1+".png");
+document.querySelector(".dice .img2").setAttribute("src", "./images/dice"+randomNumber2+".png");
+if (randomNumber1 > randomNumber2) {
+    document.querySelector(".container h1").textContent = "Player 1 wins 🚩";
+}
+else if(randomNumber1 < randomNumber2){
+    document.querySelector(".container h1").textContent = "Player 2 wins 🚩";
+}
+else{
+    document.querySelector(".container h1").textContent = "That's a draw! Try again ✨";
+}
